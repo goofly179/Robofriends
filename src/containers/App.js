9661705648e19af749a544c74a4227be65517d72
goofly179 <99113617+goofly179@users.noreply.github.com>
2022-03-10 20:31:17 -0600

    new changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -16,7 +17,7 @@ class App extends Component {
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response=> response.json())
-			.then(users => this.setState({ robots: users}));
+			.then(users => {this.setState({ robots: users})});
 	}
 
 	onSearchChange = (event) => {
@@ -35,11 +36,12 @@ class App extends Component {
 					<h1 className='f1'>RoboFriends</h1>
 					<SearchBox searchChange={this.onSearchChange}/>
 					<Scroll>
-						<CardList robots={filteredRobots} />
+						<ErrorBoundry>
+							<CardList robots={filteredRobots} />
+						</ErrorBoundry>
 					</Scroll>
 				</div>
-			);
-		
+			);	
 	}
 }
 
